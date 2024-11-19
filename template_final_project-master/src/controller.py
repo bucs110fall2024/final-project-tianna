876f1e4537e8be96a89bda3510a7effdfe688fa5
Tianna Balkam <tbalkam@binghamton.edu>
2024-11-19 02:56:24 -0500

    czxfvag

--- template_final_project-master/src/controller.py
+++ template_final_project-master/src/controller.py
@@ -1,16 +1,16 @@
-# import pygame 
-# from src.hangman import Hangman
+import pygame 
+from src.hangman import Hangman
 
-# class Controller:
-#     def __init__(self):
-#         pygame.init()
-#         pygame.event.pump()
-#         self.screen = pygame.display.set_mode()
+class Controller:
+    def __init__(self):
+        pygame.init()
+        pygame.event.pump()
+        self.screen = pygame.display.set_mode()
 
-#     def mainloop(self):
-#         while True:
-#             for event in pygame.event.get():
-#                 if event.type == pygame.MOUSEBUTTONNDOWN:
+    def mainloop(self):
+        while True:
+            for event in pygame.event.get():
+                if event.type == pygame.MOUSEBUTTONNDOWN:
                 
-#             self.hangman.draw(self.screen)
-#             pygame.display.flip()
+            self.hangman.draw(self.screen)
+            pygame.display.flip()
--- template_final_project-master/src/hangman.py
+++ template_final_project-master/src/hangman.py
@@ -1,10 +1,11 @@
-# import pygame
+import pygame
 
-# class Hangman():
-#     def __init__(self):
-#         self.image = pygame.image.load(path_to_image) 
-#         self.rect = self.image.get_rect()
-#         self.rect.x = 0
-#         self.rect.y = 0
+class Hangman():
+    def __init__(self):
+        self.image = pygame.image.load(#path_to_image) 
+        self.rect = self.image.get_rect()
+        self.rect.x = 0
+        self.rect.y = 0
 
-#     def update(self):
+    def update(self):
+        pass
