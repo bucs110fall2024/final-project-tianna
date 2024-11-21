9f0a48c6f16c92f08c200ca53d454f2d860ed782
Tianna Balkam <tbalkam@binghamton.edu>
2024-11-21 04:20:42 -0500

    xvgfdsv

--- template_final_project-master/src/game.py
+++ template_final_project-master/src/game.py
@@ -1,11 +1,31 @@
-# import pygame
+import pygame
 
-# class Game():
-
-# word: The target word for the player to guess.
-# guessed_letters: List of letters the player has guessed.
-# wrong_guesses: Number of incorrect guesses.
-
-# check_guess(letter): Validates and updates the game state based on a guessed letter.
-# is_game_over(): Checks if the game is over (win or loss).
-# get_display_word(): Returns the word with blanks and correctly guessed letters.
+class Game():
+    def __init__self(self, word, guessed_letters):
+        '''
+        Initializes the game state
+            - word: (str) The target word for the player to guess
+            - guessed_letters: (list) List of letters the player has guessed
+            = wrong_guesses: (int) Number of incorrect guesses
+        '''
+    
+    def check_guess(self, letter):
+        '''
+        Validates and updates the game state based on a guessed letter
+        args: 
+        return:
+        '''
+    
+    def is_game_over(self):
+        '''
+        Checks if the game is over (win or loss)
+        args:
+        return:
+        '''
+    
+    def get_display_word(self):
+        '''
+        Returns the word with blanks and correctly guessed letters
+        args: 
+        return:
+        '''
--- template_final_project-master/src/hangman.py
+++ template_final_project-master/src/hangman.py
@@ -1,9 +1,25 @@
-# import pygame
+import pygame
 
-# class Hangman():
-
-# images: List of hangman images.
-# current_stage: Current hangman stage based on wrong guesses.
-
-# draw(screen): Renders the current hangman stage on the screen.
-# update_stage(wrong_guesses): Updates the stage based on incorrect guesses.
+class Hangman():
+    def __init__self(self, images, current_stage):
+        '''
+        Stores the hangman drawing
+        - images: (list) List of hangman images
+        - current_stage: (int) Current hangman stage based on wrong guesses
+        args:
+        return:
+        '''
+    
+    def draw(self, screen): 
+        '''
+        Renders the current hangman stage on the screen
+        args:
+        return:
+        '''
+    
+    def update_stage(self, wrong_guesses): 
+        '''
+        Updates the stage based on incorrect guesses
+        args:
+        return:
+        '''
